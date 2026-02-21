8dfb0238e79b6898795b740cb61e0eeb8277f9da
sonubhosle <sbhosle1011@gmail.com>
2026-02-21 14:37:02 +0530

    Produts Stae

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllProducts } from '../states/Products/Action';
 
 const Home = () => {
+
+  const dispatch = useDispatch();
+  const { products, loading } = useSelector((state) => state.product);
+  console.log(products)
+  useEffect(() => {
+      console.log('Dispatching action to get products...');
+
+    dispatch(getAllProducts)
+  }, [dispatch])
   return (
     <div>Home</div>
   )
