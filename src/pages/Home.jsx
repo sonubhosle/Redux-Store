9b5da5f13ffe746912f77dac6a288f3c3ef08de6
h1 Sonu Bhosle /h1 <143890614+sonubhosle@users.noreply.github.com>
2026-02-22 21:45:40 +0530

    Clean up Home component by removing unused code
    Removed unused imports and commented-out code related to Redux.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,9 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getAllProducts } from '../states/Products/Action';
+import React from 'react'
+states/Products/Action';
 
 const Home = () => {
 
-  const dispatch = useDispatch();
-  const { products, loading } = useSelector((state) => state.product);
-  console.log(products)
-  useEffect(() => {
-      console.log('Dispatching action to get products...');
 
-    dispatch(getAllProducts)
-  }, [dispatch])
   return (
     <div>Home</div>
   )
