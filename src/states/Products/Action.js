d18bd13dd2222475302bca83258da0b99b0fd688
h1 Sonu Bhosle /h1 <143890614+sonubhosle@users.noreply.github.com>
2026-02-22 21:44:07 +0530

    em

--- src/states/Products/Action.js
+++ src/states/Products/Action.js
@@ -1,14 +1,1 @@
 
-import axios from "axios";
-import { GET_ALL_PRODUCTS_FALURE, GET_ALL_PRODUCTS_REQUEST, GET_ALL_PRODUCTS_SUCCESS } from "./Types"
-
-export const getAllProducts = () => async (dispatch) => {
-    try {
-        dispatch({ type: GET_ALL_PRODUCTS_REQUEST });
-        const res = await axios.get('http://localhost:8585/api/v1/products/');
-
-        dispatch({ type: GET_ALL_PRODUCTS_SUCCESS, payload: res.data });
-    } catch (error) {
-        dispatch({ type: GET_ALL_PRODUCTS_FALURE })
-    }
-}
